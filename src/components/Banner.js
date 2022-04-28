8ecad2a360425c5de6a04fcb9ef67e4ecf4aa5bf
Naman Singh <namanbhatia27@gmail.com>
2022-04-29 00:51:24 +0530

    moviesapp

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,14 +2,17 @@
    
 import React, { Component } from 'react'
 
+import {movies} from '../movieData'
+
 export class Banner extends Component {
   render() {
-    let backDrop =  '/6EdKBYkB1ssgGjc249ud1L5508d.jpg'
+     let moviesElem = movies.results[Math.floor((Math.random() * 10))]
+      let backDrop =  moviesElem.backdrop_path
     return (
        <div className="card banner-card" >
-       <img src="https://d2kektcjb0ajja.cloudfront.net/images/posts/feature_images/000/000/072/large-1466557422-feature.jpg?1466557422" className="card-img-top banner-img" alt="..."/>
+       <img src={`https://image.tmdb.org/t/p/original${backDrop}`} className="card-img-top banner-img" alt="..."/>
       
-         <h5 className="card-title banner-title">Oblivion</h5>
+         <h5 className="card-title banner-title">Trending Movies</h5>
          <p className="card-text  banner-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       
      </div>
